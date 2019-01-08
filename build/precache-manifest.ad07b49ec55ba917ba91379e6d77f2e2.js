67f665f4c66790e82eea4cebf520574e2ca2f271
Dimitar Dimitriev <31523385+Dimitar-sr@users.noreply.github.com>
2019-01-08 23:20:21 +0100

    Altered .gitignore file so I can commit production build to repository.

---
+++ build/precache-manifest.ad07b49ec55ba917ba91379e6d77f2e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e45a7b6527d88b1ae398",
+    "url": "/static/js/main.e45a7b65.chunk.js"
+  },
+  {
+    "revision": "a3e1b4c017b607f68dfc",
+    "url": "/static/js/1.a3e1b4c0.chunk.js"
+  },
+  {
+    "revision": "e45a7b6527d88b1ae398",
+    "url": "/static/css/main.98592a44.chunk.css"
+  },
+  {
+    "revision": "0e65c409e22d6e3653a9bbc12b7292cf",
+    "url": "/index.html"
+  }
+];
